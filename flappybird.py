9e565a0a21393a227a78a76a175684638afa5364
Kyvotcode <josuedasilva0896@gmail.com>
2025-08-12 22:08:17 -0300

    atualizada a versão no flappy bird

--- flappybird.py
+++ flappybird.py
@@ -91,7 +91,7 @@ def desenhar(self, tela):
     tela.blit(imagem_rotacionada, retangulo.topleft)
 
 def get_mask(self):
-    pygame.mask.from_surface(self.imagem)
+    return pygame.mask.from_surface(self.imagem)
 
 
 class Cano:
@@ -110,7 +110,7 @@ class Cano:
         
     def definir_altura(self):
         self.altura = random.randrange(50, 450)
-        self.pos_base = self.altura - self.CANO_TOPO.get_height()
+        self.pos_topo = self.altura - self.CANO_TOPO.get_height()
         self.pos_base = self.altura + self.DISTANCIA
         
     def mover(self):
@@ -153,9 +153,9 @@ class Chao:
         self.x2 -= self.VELOCIDADE
         
         if self.x1 + self.LARGURA < 0:
-            self.x1 = self.x1 + self.LARGURA
+            self.x1 = self.x2 + self.LARGURA
         if self.x2 + self.LARGURA < 0:
-            self.x2 = self.x2 + self.LARGURA
+            self.x2 = self.x1 + self.LARGURA
             
     def desenhar(self, tela):
         tela.blit(self.IMAGEM, (self.x1, self.y))
@@ -172,4 +172,60 @@ def desenhar_tela(tela, passaros, canos, chao, pontos):
     tela.blit(texto, (TELA_LARGURA - 10 - texto.get_width(), 10))
     chao.desenhar(tela)
     pygame.display.update()
+    
+def main():
+    passaros = [Passaro(230, 350)]
+    chao = Chao(730)
+    canos = [Cano(700)]
+    tela = pygame.display.set_mode((TELA_LARGURA, TELA_ALTURA))
+    pontos = 0
+    relogio = pygame.time.Clock()
+    
+    rodando =True
+    while rodando:
+        relogio.tick(30)
         
+        # interação com o BURRO do usuario
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                rodando = False
+                pygame.quit()
+                quit()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_SPACE:
+                    for passaro in passaros:
+                        passaro.pular()
+           
+        # mover as coisas             
+        for passaro in passaros:
+            passaro.mover()
+        chao.mover()
+        
+        adicionar_cano = False
+        for cano in canos:
+            for i, passaro in enumerate(passaros):
+                if cano.colidir(passaro):
+                    passaros.pop(i)
+                if not cano.passou and passaro.x > cano.x:
+                   cano.passou = True
+                   adicionar_cano = True
+            cano.mover() 
+            if cano.x + cano.CANO_TOPO.get_width() < 0:
+                remover_canos.append(cano)
+                
+        if adicionar_cano:
+            pontos += 1
+            canos.append(Cano(600))
+        for cano in remover_canos:
+            canos.remove(cano)
+            
+            
+        for i, passaro in enumerate(passaros):
+            if (passaro.y + passaro.imagem.get_height()) > chao.y or passaro.y < 0:
+                passaros.pop(i)
+            
+                
+        desenhar_tela(tela, passaros, canos, chao, pontos)
+        
+if __name__ == '__main__':
+    main()
